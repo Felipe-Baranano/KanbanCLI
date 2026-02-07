4e890bba6cf99d185e0bbfff40970d59b06b466f
Fellipe Alves <felipebalvesz@hotmail.com>
2026-02-07 20:51:59 -0300

    Configure app class with picocli

--- src/main/java/com/example/kanban_cli/App.java
+++ src/main/java/com/example/kanban_cli/App.java
@@ -1,10 +1,25 @@
 package com.example.kanban_cli;
 
-/**
- * Hello world!
- */
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+
+@Command(name = "kanban",
+        aliases = {"kb"},
+        description = "KanbanCLI - Manage your tasks",
+        mixinStandardHelpOptions = true,
+        version = "KanbanCLI 1.0.0",
+        subcommands = {})
+public class App implements Runnable {
+
+    @Override
+    public void run() {
+        // If no subcommand is provided, display help
+        CommandLine.usage(this, System.out);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
